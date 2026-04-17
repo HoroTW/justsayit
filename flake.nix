67f14039236a59e0edd4a516bebec68ec91e6f48
HoroTW <git@horotw.dev>
2026-04-17 19:45:34 +0200

    test: update test

--- flake.nix
+++ flake.nix
@@ -54,7 +54,7 @@
 
       mkJustsayit = { withLlm ? false, withVulkan ? false, llamaCppPython ? llama-cpp-python-new }: pkgs.python3Packages.buildPythonApplication {
         pname = "justsayit";
-        version = "0.11.8";
+        version = "0.11.9";
         pyproject = true;
 
         src = pkgs.lib.cleanSource ./.;
