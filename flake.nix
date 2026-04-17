866cc5a56f0430b3d4b7e25d14f82479c75b055e
HoroTW <git@horotw.dev>
2026-04-17 18:18:00 +0200

    chore(release): bump version to 0.11.4

--- flake.nix
+++ flake.nix
@@ -54,7 +54,7 @@
 
       mkJustsayit = { withLlm ? false, withVulkan ? false, llamaCppPython ? llama-cpp-python-new }: pkgs.python3Packages.buildPythonApplication {
         pname = "justsayit";
-        version = "0.11.3";
+        version = "0.11.4";
         pyproject = true;
 
         src = pkgs.lib.cleanSource ./.;
